d84f471de3a626055490bdde28a8179443ecc728
Rachit Kashyap <you@example.com>
2025-06-23 16:14:39 +0530

    getdata/id route add

--- server.js
+++ server.js
@@ -13,14 +13,30 @@ app.get('/getdata', async (req, res) => {
   try {
     let data = await fs.readFile(path.join(__dirname, "data.json"), "utf-8");
      data = JSON.parse(data); 
-    res.json(data);
+    res.json({success:true,data,message:"Data Fetch Success"});
   } catch (err) {
-    console.error(err);
+    console.error(err.message,"come at getdata route");
     res.status(500).send("Error reading data file");
   }
 });
 
 
+app.get('/getdata/:id', async (req, res) => {
+  try {
+    let data = await fs.readFile(path.join(__dirname, "data.json"), "utf-8");
+    data = JSON.parse(data);
+    const id = req.params.id;
+    const found = data.find(item => item.id == id);
+    if (found) {
+      res.json({success:true,message:"Data Fetch Success",data:found});
+    } else {
+      res.status(404).json({success:false,message:"Invalid id"});
+    }
+  } catch (err) {
+    console.error(err.message,"come at getdata/id route");
+    res.status(500).send("Error reading data file");
+  }
+});
 
 
 app.listen(PORT, () => {
